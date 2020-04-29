6a5344f47d80ade4192af2c63372f47ba5764824
Alvian <alvian.ardhiansyah@gmail.com>
2020-04-29 20:31:36 +0700

    first

---
+++ resources/views/frontend/main.blade.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SMP Muhammadiyah 13 Surabaya</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{asset('frontend/assets/img/logo.png')}}" rel="icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('frontend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/icofont/icofont.min.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/venobox/venobox.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+  <link href="{{asset('frontend/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+
+  <link href="{{asset('frontend/assets/css/style.css')}}" rel="stylesheet">
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <section id="topbar" class="d-none d-lg-block">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <i class="icofont-phone"></i> 031-3772661 &nbsp;
+        <i class="icofont-envelope"></i><a href="mailto:smpmuh_13sby@yahoo.com">smpmuh_13sby@yahoo.com</a>
+      </div>
+      <div class="social-links">
+        <a href="https://www.facebook.com/SMP-Muhammadiyah-13-Surabaya-1060051160727782/" class="facebook" target="blank"><i class="icofont-facebook"></i></a>
+        <a href="https://www.instagram.com/smpmuh13sby/" class="instagram" target="blank"><i class="icofont-instagram"></i></a>
+        <a href="https://www.youtube.com/channel/UCqTBxFnTPPQ1RIxiKTSrnOQ" class="youtube" target="blank"><i class="icofont-youtube"></i></a>
+        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <a href="index.html"><img src="{{asset('frontend/assets/img/logo-navbar.png')}}" class="img-fluid mt-1 ml-3" alt=""></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="drop-down"><a href="">Profile</a>
+            <ul>
+              <li><a href="tentang.html">Tentang Sekolah</a></li>
+                <li><a href="visi-misi.html">Visi Misi</a></li>
+                <li><a href="akreditasi.html">Akreditasi Sekolah</a></li>
+                <li><a href="beasiswa.html">Beasiswa</a></li>
+                <li><a href="staf.html">Stuf dan Guru</a></li>
+                <li><a href="mars.html">Mars Muhammadiyah</a></li>
+            </ul>
+          </li>
+          <li><a href="ekstrakulikuler.html">Ekstrakulikuler</a></li>
+          <li><a href="galeri.html">Galeri</a></li>
+          <li><a href="berita.html">Berita</a></li>
+          <li class="drop-down"><a href="">Pendaftaran</a>
+            <ul>
+              <li><a href="ppdb.html">Info Pendaftaran</a></li>
+              <li><a href="ppdb.html">Daftar Online</a></li>
+            </ul>
+          </li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+
+    <div class="container" data-aos="fade-up">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <h1>Hallo, Selamat Datang di SPEMGALAS</h1>
+          <p>Sekolah islam memadukan nilai-nilai tauhid ke seluruh kurikulum dan aktivitasnya </p>
+          <a href="ppdb.html" class="btn-home">Pendaftaran</a>&nbsp;&nbsp;&nbsp;&nbsp;
+          <a href="berita.html" class="btn-home2">Berita Terkini</a><br><br>
+        </div>
+        <div class="col-lg-6">
+           <link href="{{asset('frontend/assets/img/banner.png')}}" class="img-fluid" alt="">
+        </div>
+
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-xl-6 col-lg-6 d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
+            <div class="box-heading" data-aos="fade-up">
+               <link href="{{asset('frontend/assets/img/lala-1.png')}}" class="img-fluid" alt="">
+
+            </div>
+          </div>
+          <div class="col-xl-6 col-lg-6 video-box d-flex justify-content-center align-items-center py-5 px-lg-5" data-aos="zoom-in">
+            <div class="box-heading" data-aos="fade-up">
+              <h3>Kenal Lebih Dekat  </h3>
+              <p>SPEMGALAS atau SMP Muhamaad 13 Surabaya
+                berdiri sejak 1 Juli 1983 yang beralamatkan di
+                Jl. Tambak Segaran No. 27 Surabaya, belakang
+                KAZA (Kapas Krampung Plaza). </p> <br>
+
+                <a href="tentang.html" class="about btn-about">Baca Selanjutnya</a>
+            </div>
+          </div>
+        </div>
+
+          <div class="row">
+            <div class="col-xl-6 col-lg-6 video-box d-flex justify-content-center align-items-center py-5 px-lg-5" data-aos="zoom-in">
+              <div class="box-heading" data-aos="fade-up">
+                <h3>Gudang Prestasi</h3>
+                <p>SPEMGALAS menunjang lahirnya bakat dan
+                  kecerdasan siswanya. Prestasi datang dari dukungan
+                  dan doa dari berbagai pihak untuk meraih
+                  kemenangan dan kejuaraan dengan sepenuh hati </p> <br>
+
+                </div>
+              </div>
+              <div class="col-xl-6 col-lg-6 d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
+                <div class="box-heading" data-aos="fade-up">
+                   <link href="{{asset('frontend/assets/img/lala.png')}}" class="img-fluid" alt="">
+
+                </div>
+              </div>
+            </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Informasi Section ======= -->
+    <section id="berita" class="berita">
+      <div class="container">
+
+        <div class="section-title" data-aos="zoom-in">
+          <h3 align="center">
+            Berita Terkini
+            <div class="border1 mt-2" style="border-bottom: 7px solid #3B519C; width: 220px;"></div>
+          </h3>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4" data-aos="fade-up">
+            <div class="card" style="width: 20rem;">
+               <link href="{{asset('frontend/assets/img/info-1.png')}}" class="card-img-top" alt="Informasi Spemgalas">
+              <div class="card-body">
+                <h6 class="card-title">PELAKSANAAN USBN BK </h6>
+                <p class="card-text">Pelaksanaan Ujian Sekolah Berbasis Nasional –
+                  Berbasis Komputer (USBN-BK) kelas IX sedang
+                  berlangsung mulai tanggal 25 Maret – 9 April ....</p>
+                  <hr>
+                <span class="text-muted"><i class="icofont-calendar" style="color: #3B519C;"></i>&nbsp; February, 19 september 2020</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="100">
+            <div class="card" style="width: 20rem;">
+               <link href="{{asset('frontend/assets/img/info-2.png')}}" class="card-img-top" alt="Informasi Spemgalas">
+              <div class="card-body">
+                <h6 class="card-title">Penilaian Akhir Semester</h6>
+                <p class="card-text">Pelaksanaan Ujian Sekolah Berbasis Nasional –
+                  Berbasis Komputer (USBN-BK) kelas IX sedang
+                  berlangsung mulai tanggal 25 Maret – 9 April .... </p>
+                  <hr>
+                <span class="text-muted"><i class="icofont-calendar" style="color: #3B519C;"></i>&nbsp; February, 19 september 2020</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
+            <div class="card" style="width: 20rem;">
+               <link href="{{asset('frontend/assets/img/info-3.png')}}" class="card-img-top" alt="Informasi Spemgalas">
+              <div class="card-body">
+                <h6 class="card-title">SIMULASI UNBK 2019</h6>
+                <p class="card-text">Pelaksanaan Ujian Sekolah Berbasis Nasional –
+                  Berbasis Komputer (USBN-BK) kelas IX sedang
+                  berlangsung mulai tanggal 25 Maret – 9 April ....</p>
+                  <hr>
+                <span class="text-muted"><i class="icofont-calendar" style="color: #3B519C;"></i>&nbsp; February, 19 september 2020</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="lainnya" data-aos="fade-up" style="margin-top: 40px;" align="center">
+          <a href="berita.html" class="btn-berita">Lihat Lainnya</a>
+        </div>
+
+      </div>
+    </section><!-- End Why Us Section -->
+
+      <section id="bagian">
+          <div class="container">
+            <div class="row">
+              <div class="col" data-aos="fade-left">
+                <h3>Jadi Bagian dari SPEMGALAS</h3>
+              </div>
+              <div class="col" align="right" data-aos="fade-right">
+                <a href="ppdb.html" class="btn btn-bagian">Daftar Sekarang</a>
+              </div>
+            </div>
+          </div>
+      </section>
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4" align="left">
+             <link href="{{asset('frontend/assets/img/logo-2.png')}}" class="img-fluid" alt="">
+            <p>Hallo, Selamat Datang di SMP Muhammadiyah
+              13 Surabaya. Sekolah islam memadukan nilai-nilai
+              tauhid ke seluruh kurikulum dan aktivitasnya </p>
+
+              <div class="social-links">
+                <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+                <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+                <a href="#" class="youtube"><i class="icofont-youtube"></i></a>
+                <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+              </div><br><br>
+          </div>
+          <div class="col-sm-4" align="left">
+            <h5>Menu</h5><br><br>
+              <div class="row">
+                <div class="col">
+                  <a href="">Profil</a><br>
+                  <a href="">Tentang Kami</a><br>
+                  <a href="">Visi Misi</a><br>
+                  <a href="">Akreditasi</a><br>
+                  <a href="">Beasiswa</a><br>
+                  <a href="">Staff dan Guru</a><br>
+                  <a href="">Mars Muhammadiyah</a><br>
+
+                </div>
+                <div class="col">
+                  <a href="">Ekstrakulikuler</a><br><br>
+                  <a href="">Galeri</a><br><br>
+                  <a href="">Berita</a><br><br>
+                  <a href="">Pendaftaran</a><br><br>
+                </div>
+              </div>
+          </div>
+          <div class="col-sm-4" align="left">
+            <h5>Informasi</h5>
+            <p><i class="icofont-location-arrow"></i> Jl. Tambak Segaran No. 27 Surabaya<br/>
+               <i class="icofont-phone"></i> 031-3772661 <br/>
+               <i class="icofont-envelope"></i> smpmuh_13sby@yahoo.com</p>
+          </div>
+        </div>
+      </div>
+
+    </footer><!-- End Footer -->
+        <div class="akhir" align="center">
+          <small class="mt-1">2020 © All Rights Reserved Design By @Tim Elit</small>
+        </div>
+
+  <a href="#" class="back-to-top"><i class="bx bxs-up-arrow-alt"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{asset('frontend/assets/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/jquery-sticky/jquery.sticky.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/venobox/venobox.min.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/counterup/counterup.min.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('frontend/assets/vendor/aos/aos.js')}}"></script>
+
+  <script>
+    var sp = document.querySelector('.search-open');
+    var searchbar = document.querySelector('.search-inline');
+    var shclose = document.querySelector('.search-close');
+
+    function changeClass() {
+        searchbar.classList.add('search-visible');
+    }
+
+    function closesearch() {
+        searchbar.classList.remove('search-visible');
+    }
+
+    sp.addEventListener('click', changeClass);
+    shclose.addEventListener('click', closesearch);
+
+  </script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset('frontend/assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 include "admin.php";
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
+include "frontend.php";
 
 Auth::routes();
 
