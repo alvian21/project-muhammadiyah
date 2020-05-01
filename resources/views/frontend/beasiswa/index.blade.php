c68e9e4854aad36f7f238e4a26be96eafe02ec25
Alvian <alvian.ardhiansyah@gmail.com>
2020-05-01 12:28:41 +0700

    revisi

---
+++ resources/views/frontend/beasiswa/index.blade.php
@@ -0,0 +1,66 @@
+@extends('frontend.main')
+@section('main')
+<section id="hero-1">
+    <div class="hero1-container aos-init aos-animate" data-aos="fade-up">
+        <div class="container align-self-center">
+            <h1>Beasiswa SPEMGALAS</h1>
+          </div>
+    </div>
+
+
+
+</section>
+<div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb shadow p-3 mb-5 bg-white rounded">
+          <li class="breadcrumb-item"><a href="#">Profile</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Beasiswa</li>
+        </ol>
+      </nav>
+</div>
+<main id="main">
+
+    <section id="section-2">
+
+        <div class="container aos-init" data-aos="fade-up">
+            <h4><center>Beasiswa</center></h4>
+            <br><br><br>
+            <div class="container ml-3 bg-bea">
+               <div class="row">
+                 <div class="col-lg-6">
+                    <img src="./assets/img/beasiswa.png" class="img-fluid" alt=""><br><br>
+                 </div>
+                 <div class="col-lg-6 align-self-center">
+                  <ul class="list-group">
+                    <li class="list-group-item shadow-sm p-3 mb-5 bg-white rounded">
+                      <h6>Beasiswa Prestasi</h6>
+                      <p>
+                        Beasiswa ini kami berikan untuk siswa yang berprestasi atau yang mendapatkan peringkat 1 dikelas.
+                      </p>
+                    </li>
+
+                    <li class="list-group-item shadow-sm p-3 mb-5 bg-white rounded">
+                      <h6>Beasiswa Siswa Miskin (BSM)</h6>
+                      <p>
+                        Beasiswa ini berasal dari pemerintah yang diberikan untuk siswa yang mempunyai Kartu Perlindungan Sosial (KPS)
+                      </p>
+                    </li>
+
+                    <li class="list-group-item shadow-sm p-3 mb-5 bg-white rounded">
+                      <h6>Beasiswa Untuk Siswa Tidak Mampu</h6>
+                      <p>
+                        Beasiswa ini diberikan untuk siswa yang kurang mampu bahkan kita membebaskan biaya SPP untuk siswa yang tidak mampu tapi selalu berprestasi.
+                      </p>
+                    </li>
+
+
+                  </ul>
+                 </div>
+               </div>
+            </div>
+        </div>
+
+    </section>
+
+  </main>
+@endsection
--- routes/frontend.php
+++ routes/frontend.php
@@ -1,8 +1,8 @@
 <?php
 
 Route::group(['prefix' => '/', 'namespace'=>'frontend'], function () {
-
     Route::resource('/', 'HomeController');
     Route::resource('berita', 'BeritaController');
     Route::resource('akreditasi', 'AkreditasiController');
+    Route::resource('beasiswa', 'BeasiswaController');
 });
